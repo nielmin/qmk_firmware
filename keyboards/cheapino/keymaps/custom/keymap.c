7b5a9fcf9739d4e15631de82e9781816e859c578
Daniel Hwang <dawh@ines.home>
2025-05-12 03:22:32 -0500

    Update activation for toggle layer 4

--- keyboards/cheapino/keymaps/custom/keymap.c
+++ keyboards/cheapino/keymaps/custom/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_split_3x5_3(
         KC_ESC, KC_VOLD, KC_MUTE, KC_VOLU, QK_AREP,                             KC_PGUP, KC_INS, KC_HOME, KC_END, KC_DEL,
         OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), QK_REP,     KC_PGDN, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
-        KC_CAPS, TG(1), KC_NO, KC_NO, KC_NO,                                    KC_NO, KC_BSPC, KC_TAB, KC_NO, KC_ENT,
+        KC_CAPS, TG(4), KC_NO, KC_NO, KC_NO,                                    KC_NO, KC_BSPC, KC_TAB, KC_NO, KC_ENT,
             KC_TRNS, KC_NO, KC_NO,                                                  KC_NO, KC_NO, KC_NO
     ),
     [2] = LAYOUT_split_3x5_3(
