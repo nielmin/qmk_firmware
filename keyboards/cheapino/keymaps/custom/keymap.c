2875e9dcd566c6caed28d778e0af7872b9affe7c
Daniel Hwang <dawh@ines.home>
2025-05-12 01:50:38 -0500

    Enable tap dance for TAB

--- keyboards/cheapino/keymaps/custom/keymap.c
+++ keyboards/cheapino/keymaps/custom/keymap.c
@@ -10,6 +10,14 @@
  * edit it directly.
  */
 
+enum {
+    TD_TAB_ESC,
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_TAB_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_TAB, KC_ESC),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x5_3(
         KC_Q, KC_W, KC_F, KC_P, KC_B,                   KC_J, KC_L, KC_U, KC_Y, KC_QUOT,
@@ -18,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             MO(2), KC_SPC, KC_TAB,                          KC_ENT, KC_RSFT, MO(3)
     ),
     [1] = LAYOUT_split_3x5_3(
-        KC_TAB, KC_Q, KC_W, KC_E, KC_T,                 KC_Y, KC_U, KC_I, KC_O, KC_P,
+        TD(TD_TAB_ESC), KC_Q, KC_W, KC_E, KC_T,                 KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F,                KC_G, KC_H, KC_J, KC_K, KC_L,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V,                KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
             KC_LALT, KC_SPC, MO(4),                         KC_ENT, KC_RSFT, MO(2)
