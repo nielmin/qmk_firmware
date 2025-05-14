efbfb2d53d2d71d0011e62714168f587d0454f60
Daniel Hwang <daniel@asta.nix>
2025-05-14 15:47:39 -0500

    Add layer tap on base tab key to numbers

--- keyboards/cheapino/keymaps/custom/keymap.c
+++ keyboards/cheapino/keymaps/custom/keymap.c
@@ -23,13 +23,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q, KC_W, KC_F, KC_P, KC_B,                   KC_J, KC_L, KC_U, KC_Y, KC_QUOT,
         KC_A, KC_R, KC_S, KC_T, KC_G,                   KC_M, KC_N, KC_E, KC_I, KC_O,
         KC_Z, ALT_T(KC_X), CTL_T(KC_C), KC_D, KC_V,     KC_K, KC_H, CTL_T(KC_COMM), ALT_T(KC_DOT), KC_SLSH,
-            MO(2), KC_SPC, KC_TAB,                          KC_ENT, KC_RSFT, MO(3)
+            MO(2), KC_SPC, LT(4, KC_TAB),                          KC_ENT, KC_RSFT, MO(3)
     ),
     [1] = LAYOUT_split_3x5_3(
         TD(TD_TAB_ESC), KC_Q, KC_W, KC_E, KC_T,                 KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F,                KC_G, KC_H, KC_J, KC_K, KC_L,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V,                KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
-            KC_LALT, KC_SPC, MO(4),                         KC_ENT, KC_RSFT, MO(2)
+            KC_LALT, KC_SPC, KC_NO,                         KC_ENT, KC_RSFT, MO(2)
     ),
     [2] = LAYOUT_split_3x5_3(
         KC_ESC, KC_VOLD, KC_MUTE, KC_VOLU, QK_AREP,                             KC_PGUP, KC_INS, KC_HOME, KC_END, KC_DEL,
