44f73c389d7c237fc4d3531a54200c0acb9dcb7b
Daniel Hwang <daniel@asta.nix>
2025-05-17 15:46:22 -0500

    Remove btm row HRMs

--- keyboards/cheapino/keymaps/nielmin/keymap.c
+++ keyboards/cheapino/keymaps/nielmin/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x5_3(
         KC_Q, KC_W, KC_F, KC_P, KC_B,                   KC_J, KC_L, KC_U, KC_Y, KC_QUOT,
         KC_A, KC_R, KC_S, KC_T, KC_G,                   KC_M, KC_N, KC_E, KC_I, KC_O,
-        KC_Z, ALT_T(KC_X), CTL_T(KC_C), KC_D, KC_V,     KC_K, KC_H, CTL_T(KC_COMM), ALT_T(KC_DOT), KC_SLSH,
+        KC_Z, KC_X, KC_C, KC_D, KC_V,                   KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH,
             MO(1), KC_SPC, KC_TAB,                          KC_ENT, KC_RSFT, MO(2)
     ),
     /*
