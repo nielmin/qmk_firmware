2a46b1e84ba774394c1cce1988c97c7e6b7a0173
Daniel Hwang <dawh@ines.home>
2025-05-28 16:36:45 -0500

    Fix typo in keymap

--- keyboards/cheapino/keymaps/nielmin/keymap.c
+++ keyboards/cheapino/keymaps/nielmin/keymap.c
@@ -21,7 +21,7 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x5_3(
         KC_Q, KC_W, KC_F, KC_P, KC_B,                                           KC_J, KC_L, KC_U, KC_Y, KC_QUOT,
-        KC_A, LALT_T(KC_R, LCTL_T(KC_S), LSFT_T(KC_T), KC_G,                    KC_M, RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), KC_O,
+        KC_A, LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G,                   KC_M, RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), KC_O,
         KC_Z, KC_X, KC_C, KC_D, KC_V,                                           KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH,
             MO(1), KC_SPC, KC_TAB,                                              RSFT_T(KC_ENT), LT(3, KC_BSPC), MO(2)
     ),
