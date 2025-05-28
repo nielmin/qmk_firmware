5c16ef366e53ce8e0942ec6cc87b2414cf25a16b
Daniel Hwang <dawh@ines.home>
2025-05-28 15:43:34 -0500

    Swap alt and shift on gaming layer

--- keyboards/cheapino/keymaps/nielmin/keymap.c
+++ keyboards/cheapino/keymaps/nielmin/keymap.c
@@ -46,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [4] = LAYOUT_split_3x5_3(
         KC_TAB, KC_Q, KC_W, KC_E, KC_R,                 KC_T, KC_Y, KC_U, KC_I, KC_O,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F,                KC_G, KC_H, KC_J, KC_K, KC_L,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V,                KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
-            MO(5), KC_SPC, KC_LALT,                         KC_ENT, KC_RSFT, MO(2)
+        KC_LALT, KC_Z, KC_X, KC_C, KC_V,                KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
+            MO(5), KC_SPC, KC_LSFT,                         KC_ENT, KC_BSPC, MO(2)
     ),
     [5] = LAYOUT_split_3x5_3(
         KC_ESC, KC_1, KC_2, KC_3, KC_4,                 KC_Y, KC_U, KC_I, KC_O, KC_P,
