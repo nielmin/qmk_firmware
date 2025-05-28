262ad4f76fec3aefd6635f99e5a359446ab4d489
Daniel Hwang <dawh@ines.home>
2025-05-28 16:03:57 -0500

    Update keymap with mod tap shift/enter

--- keyboards/cheapino/keymaps/nielmin/keymap.c
+++ keyboards/cheapino/keymaps/nielmin/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q, KC_W, KC_F, KC_P, KC_B,                   KC_J, KC_L, KC_U, KC_Y, KC_QUOT,
         KC_A, KC_R, KC_S, KC_T, KC_G,                   KC_M, KC_N, KC_E, KC_I, KC_O,
         KC_Z, KC_X, KC_C, KC_D, KC_V,                   KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH,
-            MO(1), KC_SPC, KC_TAB,                          KC_ENT, KC_RSFT, MO(2)
+            MO(1), KC_SPC, KC_TAB,                          MT(MOD_RSFT, KC_ENT), KC_RSFT, MO(2)
     ),
     [1] = LAYOUT_split_3x5_3(
         KC_ESC, KC_VOLD, KC_MUTE, KC_VOLU, QK_AREP,                             KC_PGUP, KC_INS, KC_HOME, KC_END, KC_DEL,
