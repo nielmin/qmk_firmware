6fd9c353dba3649586f3cc627bbf7f0ec0a8dc6b
Daniel Hwang <dawh@ines.home>
2025-05-29 12:57:34 -0500

    Revert "Update keymap with layer tap on R mid thumb"
    This reverts commit 13a5a675f9f51bf7236c4a2e087b7e67d46a3cf2.

--- keyboards/cheapino/keymaps/nielmin/keymap.c
+++ keyboards/cheapino/keymaps/nielmin/keymap.c
@@ -23,19 +23,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q, KC_W, KC_F, KC_P, KC_B,                   KC_J, KC_L, KC_U, KC_Y, KC_QUOT,
         KC_A, KC_R, KC_S, KC_T, KC_G,                   KC_M, KC_N, KC_E, KC_I, KC_O,
         KC_Z, KC_X, KC_C, KC_D, KC_V,                   KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH,
-            MO(1), KC_SPC, KC_TAB,                          MT(MOD_RSFT, KC_ENT), LT(3, KC_BSPC), MO(2)
+            MO(1), KC_SPC, KC_TAB,                          MT(MOD_RSFT, KC_ENT), KC_RSFT, MO(2)
     ),
     [1] = LAYOUT_split_3x5_3(
         KC_ESC, KC_VOLD, KC_MUTE, KC_VOLU, QK_AREP,                             KC_PGUP, KC_INS, KC_HOME, KC_END, KC_DEL,
         OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), QK_REP,     KC_PGDN, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
-        KC_NO, TG(4), KC_NO, KC_NO, KC_NO,                                      KC_NO, KC_BSPC, KC_TAB, KC_NO, KC_ENT,
-            KC_TRNS, KC_NO, KC_NO,                                                  KC_NO, KC_NO, KC_NO
+        KC_NO, TG(4), KC_NO, KC_NO, KC_CAPS,                                    KC_NO, KC_BSPC, KC_TAB, KC_NO, KC_ENT,
+            KC_TRNS, KC_NO, KC_NO,                                                  KC_NO, KC_NO, MO(3)
     ),
     [2] = LAYOUT_split_3x5_3(
         KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, KC_SCLN,
         KC_GRV, KC_TILD, KC_LCBR, KC_LPRN, KC_LBRC,     KC_COLN, OSM(MOD_RSFT), OSM(MOD_RCTL), OSM(MOD_RALT), OSM(MOD_RGUI),
         KC_LT, KC_GT, KC_RCBR, KC_RPRN, KC_RBRC,        KC_PIPE, KC_MINS, KC_EQL, KC_PLUS, KC_BSLS,
-            KC_NO, KC_NO,KC_NO,                             KC_NO, KC_NO, KC_TRNS
+            MO(3), KC_NO,KC_NO,                             KC_NO, KC_NO, KC_TRNS
     ),
     [3] = LAYOUT_split_3x5_3(
         KC_1, KC_2, KC_3, KC_4, KC_5,                                           KC_6, KC_7, KC_8, KC_9, KC_0,
