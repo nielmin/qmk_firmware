6bc0a643a4591dc8f2fff690dda7c7eea4739995
Daniel Hwang <dawh@ines.home>
2025-06-03 18:54:07 -0500

    Update keymap: gaming layers

--- keyboards/cheapino/keymaps/nielmin/keymap.c
+++ keyboards/cheapino/keymaps/nielmin/keymap.c
@@ -57,12 +57,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R,                 KC_T, KC_Y, KC_U, KC_I, KC_O,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F,                KC_G, KC_H, KC_J, KC_K, KC_L,
         SFT_T(KC_LALT), KC_Z, KC_X, KC_C, KC_V,         KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
-            MO(5), SFT_T(KC_SPC), KC_LALT,                    KC_ENT, KC_BSPC, MO(2)
+            MO(5), KC_SPC, KC_LSFT,                         KC_ENT, KC_BSPC, MO(2)
     ),
     [_GAM2] = LAYOUT_split_3x5_3(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4,                 KC_Y, KC_U, KC_I, KC_O, KC_P,
+        KC_ESC, KC_1, KC_2, KC_3, KC_T,                 KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_P, KC_O, KC_J, KC_H, KC_G,                   KC_G, KC_H, KC_J, KC_K, KC_L,
-        KC_I, TG(4), KC_N, KC_M, KC_B,               KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
+        KC_O, KC_I, KC_N, KC_M, KC_B,                   KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
             KC_NO, KC_NO, KC_NO,                            KC_NO, KC_NO, KC_NO
     ),
 };
