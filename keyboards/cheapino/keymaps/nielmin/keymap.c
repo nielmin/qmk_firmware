1cd3e932f0d479ad7e10712477e15d396becf157
Daniel Hwang <dawh@ines.home>
2025-06-03 19:04:40 -0500

    Update keymap: Add reset key

--- keyboards/cheapino/keymaps/nielmin/keymap.c
+++ keyboards/cheapino/keymaps/nielmin/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_1, KC_2, KC_3, KC_4, KC_5,                                           KC_6, KC_7, KC_8, KC_9, KC_0,
         OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), KC_F11,     KC_F12, OSM(MOD_RSFT), OSM(MOD_RCTL), OSM(MOD_RALT), OSM(MOD_RGUI),
         KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                      KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
-            KC_TRNS, KC_NO, KC_TRNS,                                                KC_NO, KC_NO, KC_TRNS
+            KC_TRNS, KC_NO, KC_TRNS,                                                KC_NO, QK_BOOT, KC_TRNS
     ),
     [_GAM1] = LAYOUT_split_3x5_3(
         KC_TAB, KC_Q, KC_W, KC_E, KC_R,                 KC_T, KC_Y, KC_U, KC_I, KC_O,
