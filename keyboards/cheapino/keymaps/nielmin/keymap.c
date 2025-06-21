0c7131f5bf010e2af05a87362c6c3bbb750fb5ba
Daniel Hwang <dawh@ines.home>
2025-06-21 18:09:52 -0500

    Update keymap GAM1 layer

--- keyboards/cheapino/keymaps/nielmin/keymap.c
+++ keyboards/cheapino/keymaps/nielmin/keymap.c
@@ -63,10 +63,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS, KC_NO, KC_TRNS,                                                KC_NO, QK_BOOT, KC_TRNS
     ),
     [_GAM1] = LAYOUT_split_3x5_3(
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R,                 KC_T, KC_Y, KC_U, KC_I, KC_O,
-        KC_LCTL, KC_A, KC_S, KC_D, KC_F,                KC_G, KC_H, KC_J, KC_K, KC_L,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V,                KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
-            MO(5), KC_SPC, KC_LALT,                         KC_ENT, KC_BSPC, KC_NO
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F,                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V,                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            MO(5), KC_SPC, LT(_GAM3,KC_LALT),           KC_ENT, KC_BSPC, KC_NO
     ),
     [_GAM2] = LAYOUT_split_3x5_3(
         KC_ESC, KC_1, KC_2, KC_3, KC_T,                 KC_Y, KC_U, KC_I, KC_O, KC_P,
