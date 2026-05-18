dfd02eeb6b37e0f9efaaf36d1b6c91a65c9e8ee9
Daniel Hwang <whadmia@gmail.com>
2026-05-18 16:45:30 -0500

    Set tapping term to 180ms

--- keyboards/cheapino/config.h
+++ keyboards/cheapino/config.h
@@ -26,11 +26,12 @@
 
 
 // Pick good defaults for enabling homerow modifiers
-//#define QUICK_TAP_TERM 0
-// #define TAPPING_TERM 210
+// #define QUICK_TAP_TERM 0
+#define TAPPING_TERM 180
 // #define CHORDAL_HOLD
 // #define PERMISSIVE_HOLD
 // #define TAP_FLOW_TERM 150
+//
 
 
 #define WS2812_DI_PIN GP16 // The pin connected to the data pin of the LEDs
