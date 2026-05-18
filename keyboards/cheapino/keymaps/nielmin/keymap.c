922c87e668d955f069bb4798adac3ba5e0b91fd1
Daniel Hwang <whadmia@gmail.com>
2026-05-18 16:41:30 -0500

    Reformat

--- keyboards/cheapino/keymaps/nielmin/keymap.c
+++ keyboards/cheapino/keymaps/nielmin/keymap.c
@@ -23,36 +23,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC, KC_VOLD, KC_MUTE, KC_VOLU, QK_AREP,                             KC_PGUP, KC_INS, KC_HOME, KC_END, KC_DEL,
         OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), QK_REP,     KC_PGDN, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
         KC_UNDO, KC_CUT, KC_COPY, KC_TAB, KC_PSTE,                              KC_NO, KC_BSPC, KC_TAB, KC_NO, KC_ENT,
-            KC_TRNS, KC_NO, KC_NO,                                              KC_NO, KC_NO, MO(3)
+            KC_TRNS, KC_NO, KC_NO,                                                  KC_NO, KC_NO, MO(3)
     ),
     [_SYM] = LAYOUT_split_3x5_3(
-        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, KC_SCLN,
-        KC_GRV, KC_TILD, KC_LCBR, KC_LPRN, KC_LBRC,     KC_COLN, OSM(MOD_RSFT), OSM(MOD_RCTL), OSM(MOD_RALT), OSM(MOD_RGUI),
-        KC_LT, KC_GT, KC_RCBR, KC_RPRN, KC_RBRC,        KC_PIPE, KC_MINS, KC_EQL, KC_PLUS, KC_BSLS,
-            MO(3), KC_NO,KC_NO,                             KC_NO, KC_NO, KC_TRNS
+        KC_1, KC_2, KC_3, KC_4, KC_5,                                           KC_6, KC_7, KC_8, KC_9, KC_0,
+        KC_GRV, KC_TILD, KC_LCBR, KC_LPRN, KC_LBRC,                             KC_COLN, OSM(MOD_RSFT), OSM(MOD_RCTL), OSM(MOD_RALT), OSM(MOD_RGUI),
+        KC_LT, KC_GT, KC_RCBR, KC_RPRN, KC_RBRC,                                KC_PIPE, KC_MINS, KC_EQL, KC_PLUS, KC_BSLS,
+            MO(3), KC_NO,KC_NO,                                                     KC_NO, KC_NO, KC_TRNS
     ),
     [_NUM] = LAYOUT_split_3x5_3(
-        KC_1, KC_2, KC_3, KC_4, KC_5,                                           KC_6, KC_7, KC_8, KC_9, KC_0,
+        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                               KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, KC_SCLN,
         OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), KC_F11,     KC_F12, OSM(MOD_RSFT), OSM(MOD_RCTL), OSM(MOD_RALT), OSM(MOD_RGUI),
         KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                      KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
             KC_TRNS, KC_NO, KC_TRNS,                                                KC_NO, QK_BOOT, KC_TRNS
     ),
-    // [_GAM1] = LAYOUT_split_3x5_3(
-    //     KC_TAB, KC_Q, KC_W, KC_E, KC_R,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    //     KC_LCTL, KC_A, KC_S, KC_D, KC_F,                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    //     KC_LSFT, KC_Z, KC_X, KC_C, KC_V,                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    //         MO(5), KC_SPC, LT(_GAM3,KC_LALT),           KC_ENT, KC_BSPC, KC_NO
-    // ),
-    // [_GAM2] = LAYOUT_split_3x5_3(
-    //     KC_ESC, KC_1, KC_2, KC_3, KC_T,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    //     KC_L, KC_K, KC_J, KC_H, KC_G,                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    //     KC_P, KC_O, KC_I, KC_M, KC_B,                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    //         KC_NO, KC_NO, KC_NO,                            KC_NO, KC_NO, TG(4)
-    // ),
-    // [_GAM3] = LAYOUT_split_3x5_3(
-    //     KC_ESC, KC_4, KC_5, KC_6, KC_Y,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    //     KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_F5,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    //     KC_F1, KC_F2, KC_F3, KC_F4, KC_F9,              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    //         KC_NO, KC_NO, KC_NO,                            KC_NO, KC_NO, KC_NO
-    // ),
 };
