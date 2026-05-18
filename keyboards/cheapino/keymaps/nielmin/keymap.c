89aeb08f58faaf75104ea89f0268953623816266
Daniel Hwang <whadmia@gmail.com>
2026-05-18 17:32:08 -0500

    Fix duplicate key in base layer

--- keyboards/cheapino/keymaps/nielmin/keymap.c
+++ keyboards/cheapino/keymaps/nielmin/keymap.c
@@ -14,7 +14,7 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x5_3(
-        KC_B, KC_L, KC_D, KC_C, KC_V,                                           KC_J, KC_F, KC_O, KC_U, KC_QUOT,
+        KC_B, KC_L, KC_D, KC_C, KC_V,                                           KC_J, KC_Y, KC_O, KC_U, KC_QUOT,
         KC_N, KC_R, KC_T, KC_S, KC_G,                                           KC_P, KC_H, KC_A, KC_E, KC_I,
         KC_X, KC_Q, KC_M, KC_W, KC_Z,                                           KC_K, KC_F, KC_COMM, KC_DOT, KC_SLSH,
             MO(1), KC_SPC, KC_NO,                                                   KC_NO, KC_RSFT, MO(2)
